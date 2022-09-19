d428c753874e2da2c334c82298c60d9e061df197
Bambo-Borris <canyoufeelit4@gmail.com>
2022-09-19 15:17:02 +0100

    Setup of scene & rune texture

--- src/RuneRemember.hpp
+++ src/RuneRemember.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <array>
 
 class RuneRemember {
 public:
@@ -10,4 +13,7 @@ public:
 
 private:
     sf::RenderWindow m_window;
+    sf::Texture m_runeSheet;
+    sf::Sprite m_activeRune;
+    size_t m_runeIndex { 0 };
 };
