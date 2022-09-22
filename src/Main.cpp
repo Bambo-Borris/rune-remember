03beb063507ded5fd36ef8b0ff15c7868553c0fc
Bambo-Borris <canyoufeelit4@gmail.com>
2022-09-22 05:23:47 +0100

    Load rune information from disk

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,6 +5,8 @@ SFML_DEFINE_DISCRETE_GPU_PREFERENCE
 
 int main(int argc, char* argv[])
 {
+    (void)argc;
+    (void)argv;
     RuneRemember rr;
     rr.run();
 }
--- src/RuneRemember.cpp
+++ src/RuneRemember.cpp
@@ -1,17 +1,30 @@
 #include "RuneRemember.hpp"
 
+#include <SFML/Graphics/Text.hpp>
 #include <SFML/System/String.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/VideoMode.hpp>
+#include <spdlog/spdlog.h>
+
+#include <fstream>
 
 constexpr auto RUNE_IMAGE_SIZE { sf::Vector2i { 32, 32 } };
 
+inline void CentreTextOrigin(sf::Text& text)
+{
+    const auto localBounds = sf::Vector2f(text.getLocalBounds().left, text.getLocalBounds().top);
+    const auto globalOrigin = sf::Vector2f(text.getGlobalBounds().width / 2.0f, text.getGlobalBounds().height / 2.0f);
+
+    text.setOrigin(localBounds + globalOrigin);
+}
+
 RuneRemember::RuneRemember()
 {
+    spdlog::set_level(spdlog::level::debug);
+
     sf::ContextSettings ctxt;
     ctxt.antialiasingLevel = 16;
-    // m_window.create(sf::VideoMode({ 600, 600 }), "Hello World", sf::Style::Default);
-    m_window.create(sf::VideoMode({ 800, 600 }), "WINDOW_TITLE", sf::Style::Default, ctxt);
+    m_window.create(sf::VideoMode({ 800, 600 }), "Rune Remember!", sf::Style::Default, ctxt);
     m_window.setFramerateLimit(30);
 
     if (!m_runeSheet.loadFromFile("bin/textures/runes.png")) {
@@ -23,6 +36,17 @@ RuneRemember::RuneRemember()
     m_activeRune.setScale({ 10, 10 });
     m_activeRune.setOrigin(sf::Vector2f { RUNE_IMAGE_SIZE } * 0.5f);
     m_activeRune.setPosition(sf::Vector2f { m_window.getSize() } * 0.5f);
+
+    if (!m_mainFont.loadFromFile("bin/fonts/Montserrat-Medium.ttf")) {
+        throw std::runtime_error("Unable to load main font");
+    }
+
+    if (!loadRuneInfoFromDisk()) {
+        throw std::runtime_error("Unable to load rune info");
+    }
+
+    m_questionText.setFont(m_mainFont);
+    m_questionText.setString(m_runesInfo[0].name);
 }
 
 void RuneRemember::run()
@@ -36,10 +60,38 @@ void RuneRemember::run()
         while (m_window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 m_window.close();
+
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Escape)
+                    m_window.close();
+            }
         }
 
         m_window.clear({ 175, 175, 175 });
         m_window.draw(m_activeRune);
+        m_window.draw(m_questionText);
         m_window.display();
     }
+}
+
+bool RuneRemember::loadRuneInfoFromDisk()
+{
+    std::ifstream runeInfoFile;
+    runeInfoFile.open("bin/data/rune_info.txt");
+
+    if (runeInfoFile.fail()) {
+        return false;
+    }
+
+    size_t index = 0;
+    while (!runeInfoFile.eof()) {
+        std::string data;
+        runeInfoFile >> data;
+        m_runesInfo.push_back({
+            index,
+            sf::String::fromUtf8(data.begin(), data.end()),
+        });
+        ++index;
+    }
+    return true;
 }
