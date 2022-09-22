ae702b01a0c6da96e125670822292ea8b02d196c
Bambo-Borris <canyoufeelit4@gmail.com>
2022-09-22 06:27:38 +0100

    Setup app mode selection menu

--- src/RuneRemember.hpp
+++ src/RuneRemember.hpp
@@ -19,13 +19,27 @@ private:
         sf::String name;
     };
 
+    enum class AppMode { SelectMode, RandomTest, SlowRunThrough, Complete };
+
     bool loadRuneInfoFromDisk();
+    void update(const sf::Time& dt);
+    void setupSelectMode();
+    void drawSelectMode();
+    void updateSelectMode();
+    void handleEventSelectMode(const sf::Event& event);
 
     sf::RenderWindow m_window;
     sf::Texture m_runeSheet;
     sf::Sprite m_activeRune;
     sf::Text m_questionText;
+    sf::Text m_selectModeTitleText;
+    sf::Text m_selectOption1;
+    sf::Text m_selectOption2;
     sf::Font m_mainFont;
+
     std::vector<Rune> m_runesInfo;
     size_t m_runeIndex { 0 };
+    sf::Vector2f m_mousePosition;
+
+    AppMode m_currentMode { AppMode::SelectMode };
 };
